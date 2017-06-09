ce34143384dafd5f91ea8340c87fdaa0819a2f88
teacher1 <teacher1@TUT-NMORA>
2017-06-09 09:53:32 +0300

    Add project files.

---
+++ Angular2Demo/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home-page',
+    template: `<h1>Hello {{home}}</h1>`,
+})
+export class HomeComponent {
+    home = 'it works!';
+}
+
+
